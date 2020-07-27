1d54e9fca2b0bd150d341def849ac81335af39cd
Daniel Matilla <daniel@esdis.es>
2020-07-27 11:20:18 +0200

    Fix bug with FF not displaying images

--- controllers/image.go
+++ controllers/image.go
@@ -19,6 +19,8 @@ func (c *ImageController) Get() {
 	str, _ := base64.StdEncoding.DecodeString(aux[0])
 	response, _ := http.Get(fmt.Sprintf("%s", str))
 	raw, _ := ioutil.ReadAll(response.Body)
+	ct := http.DetectContentType(raw)
 	c.Ctx.Output.Header("Cache-Control", "max-age=31536000")
+	c.Ctx.Output.Header("Content-Type", ct)
 	c.Ctx.Output.Body(raw)
 }
